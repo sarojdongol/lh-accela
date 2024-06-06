f0b090c6a5226dfcc23aaf0bcd92f2d66823f48c
saroj <saroj.dongol@ir.com>
2024-06-06 20:24:49 +1000

    base infra

--- base_infra/main.tf
+++ base_infra/main.tf
@@ -1,5 +1,5 @@
 module "oidc" {
-  source              = "./modules/oidc_setup"
+  source              = "../modules/oidc_setup/"
   circleci_org_id     = "59d58a9e-1d8e-4430-b31f-df1ad3abc3d7"
   circleci_project_id = "26d11f32-fded-45c1-915e-cd8023f53d27"
 }
