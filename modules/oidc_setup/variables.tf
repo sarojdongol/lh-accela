98a07955e40b99f5596266e3dab750ad7274a2c6
saroj <saroj.dongol@ir.com>
2024-06-09 00:01:12 +1000

    fmt

--- modules/oidc_setup/variables.tf
+++ modules/oidc_setup/variables.tf
@@ -1,13 +1,13 @@
 variable "circleci_org_id" {
   type        = string
   description = "The UUID formatted Organization ID from CircleCI"
-  default = "59d58a9e-1d8e-4430-b31f-df1ad3abc3d7"
+  default     = "59d58a9e-1d8e-4430-b31f-df1ad3abc3d7"
 }
 
 variable "circleci_project_id" {
   type        = string
   description = "The UUID formatted Project ID from CircleCI"
-  default = "26d11f32-fded-45c1-915e-cd8023f53d27"
+  default     = "26d11f32-fded-45c1-915e-cd8023f53d27"
 }
 
 variable "thumbprints" {
